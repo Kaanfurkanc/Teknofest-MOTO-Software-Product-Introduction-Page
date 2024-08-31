f8e36a2dca3313bf958347bd21df4d83ce2e73e0
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2024-08-31 17:27:20 +0300

    Last Version Of ProductWebsite

--- PsiKodWebPage/js/script.js
+++ PsiKodWebPage/js/script.js
@@ -1,11 +1,56 @@
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
+// EmailJS kütüphanesini başlatma
+(function(){
+    emailjs.init("HZBccd8rHvTdApjuR"); // Public Key'inizi buraya ekleyin
+})();
 
-    const emailBody = `Name: ${name}\nEmail: ${email}\nMessage: ${message}`;
-    
-    window.location.href = `mailto:psikod.iau@gmail.com?subject=Contact Form Submission&body=${encodeURIComponent(emailBody)}`;
+// Formun submit olayı için event listener ekleme
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Sayfanın yeniden yüklenmesini engeller
+
+        // Form verilerini gönderme
+        emailjs.sendForm('service_jksrmfx', 'template_knfclhc', this)
+            .then(function(response) {
+                alert('Mesajınız başarıyla gönderildi!', response.status, response.text);
+            }, function(error) {
+                alert('Mesaj gönderilirken bir hata oluştu...', error);
+            });
+    });
 });
+
+// Textarea otomatik boyutlandırma
+const textarea = document.getElementById('message');
+textarea.addEventListener('input', function () {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
+
+// Modal ve checkbox işlevselliği
+var modal = document.getElementById("termsModal");
+var termsLabel = document.getElementById("terms-label");
+var span = document.getElementsByClassName("close")[0];
+var acceptBtn = document.getElementById("acceptBtn");
+var termsCheckbox = document.getElementById("terms");
+
+// Label tıklanırsa, modalı göster
+termsLabel.onclick = function() {
+    modal.style.display = "block";
+}
+
+// X tıklanırsa, modalı kapat
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Kabul et butonuna tıklanırsa, checkbox işaretlenir ve modal kapanır
+acceptBtn.onclick = function() {
+    termsCheckbox.checked = true;
+    modal.style.display = "none";
+}
+
+// Kullanıcı modaldan başka bir yere tıklarsa, modalı kapat
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
